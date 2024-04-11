7fa67226b6f09c6cadc0bccff242fac48985074f
chul5 <och7164@gmail.com>
2024-04-11 11:17:54 +0900

    feat: implement modify UserInfo

---
+++ src/main/java/com/olive/servlet/UserModifyServlet.java
@@ -0,0 +1,43 @@
+package com.olive.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.olive.dao.UserDAO;
+import com.olive.service.UserService;
+import com.olive.vo.User;
+
+/**
+ * Servlet implementation class UserModifyServlet
+ */
+@WebServlet("/user/modify")
+public class UserModifyServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+    private UserService userService;
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public UserModifyServlet() {
+    	userService = new UserService(new UserDAO());
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		User user = userService.toUserVO(request);
+	}
+
+}
